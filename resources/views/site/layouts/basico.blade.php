c14b91db2138ef77f8a921838b0f454060569f43
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-09 18:37:21 -0300

    include - navbar

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="{{asset('css/estilo_basico.css')}}">
     </head>
     <body>
+        @include('site.layouts._partials.topo')
         @yield('conteudo')
     </body>
     </html>
