23c8072a65d3124ddef2fe913b01e10a29d89928
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-10 16:41:50 -0300

    Alterando uma migration sem apagar dados do banco

---
+++ database/migrations/2021_12_10_193706_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //table seleciona a tabela para fazer uma alteração -- usa isso para não perder os dados já existentes
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
