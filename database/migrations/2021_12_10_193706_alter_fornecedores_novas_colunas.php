d582ef8f086ef350869cd9c6b443c3a340565202
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-10 17:43:23 -0300

    nullable e dafault

--- database/migrations/2021_12_10_193706_alter_fornecedores_novas_colunas.php
+++ database/migrations/2021_12_10_193706_alter_fornecedores_novas_colunas.php
@@ -27,6 +27,8 @@ class AlterFornecedoresNovasColunas extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropColumn(['uf', 'email']);
+        });
     }
 }
