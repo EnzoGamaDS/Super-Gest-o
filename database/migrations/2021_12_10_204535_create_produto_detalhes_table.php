cc827f1342c558c98cb0682d45f60e50273709a5
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-10 18:07:47 -0300

    1:1 produtos e produto_detalhes

---
+++ database/migrations/2021_12_10_204535_create_produto_detalhes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            //colunas
+            $table->id();
+            $table->unsignedBigInteger('produto_id'); //atenção o tipo da chave estrangeira é unsignedBigInteger
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->unique('produto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
