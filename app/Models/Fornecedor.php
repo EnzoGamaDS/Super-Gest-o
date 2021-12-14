b1230359d8374899a8a6341c1bd9714e5ea4d9a6
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-13 22:32:19 -0300

    SoftDelete

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Fornecedor extends Model
 {
+    //Soft Deletes
+    use SoftDeletes;
     use HasFactory;
+    //
+    protected $table = 'fornecedores';
+
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//O padrão de reconhecimento do laravel é buscar a tabela com o nome do Model usando underline e caixa baixa
+//ex SiteContato == site_contatos
 class SiteContato extends Model
 {
     use HasFactory;
+
+
 }
