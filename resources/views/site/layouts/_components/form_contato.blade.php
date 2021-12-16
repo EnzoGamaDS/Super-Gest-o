ded156b31eca462954b339333bec4e0074eda77a
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-15 21:09:34 -0300

    Tratamento de erros - recuperando dados com old

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,20 +1,26 @@
 {{$slot}}
 <form action="{{route('site.contato')}}" method="POST">
     @csrf {{--Token para que o metodo post funcione--}}
-    <input name="nome" type="text" placeholder="Nome" class="{{$classe}}">
+    <input name="nome" value="{{old('nome')}}" type="text" placeholder="Nome" class="{{$classe}}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{$classe}}">
+    <input name="telefone"value="{{old('telefone')}}"  type="text" placeholder="Telefone" class="{{$classe}}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{$classe}}">
+    <input name="email" value="{{old('email')}}" type="text" placeholder="E-mail" class="{{$classe}}">
     <br>
-    <select name="motivo_contato" class="{{$classe}}">
+    <select name="motivo_contato"  class="{{$classe}}">
         <option value="">Qual o motivo do contato?</option>
         <option value="1">Dúvida</option>
         <option value="2">Elogio</option>
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="{{$classe}}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{$classe}}">
+        @if (old('mensagem') != '')
+            {{old('mensagem')}}
+            @else
+                 Preencha aqui a sua mensagem
+            @endif
+    </textarea>
     <br>
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
