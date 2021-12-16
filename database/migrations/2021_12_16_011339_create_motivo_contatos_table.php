2acac74b36d2ecf8c69eb85103946c8291423176
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-15 22:19:12 -0300

    Usando dados persistidos no banco

---
+++ database/migrations/2021_12_16_011339_create_motivo_contatos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\MotivoContato;
+
+class CreateMotivoContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('motivo_contatos', function (Blueprint $table) {
+            $table->id();
+            $table->string('motivo_contato', 20);
+            $table->timestamps();
+        });
+
+        MotivoContato::create(['Dúvida']);
+        MotivoContato::create(['Elogio']);
+        MotivoContato::create(['Reclamação']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('motivo_contatos');
+    }
+}
