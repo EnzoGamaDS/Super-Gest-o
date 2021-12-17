9be7602c680cf47f04e649e94c7315f3e8424f49
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-16 21:16:19 -0300

    Apelidando middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\logAcessoMiddleware::class
+            //\App\Http\Middleware\logAcessoMiddleware::class
         ],
 
         'api' => [
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'log.acesso' => \App\Http\Middleware\logAcessoMiddleware::class
+
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index')->middleware('log.acesso');
 Route::get('/sobre-nos',[SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato',[ContatoController::class, 'contato'])->name('site.contato');
 
-Route::get('/contato',[ContatoController::class, 'salvar'])->name('site.contato');
+Route::get('/contato',[ContatoController::class, 'salvar'])->name('site.contato')->middleware('log.acesso');
 
 Route::get('login/', function () {return 'login';})->name('site.login');
 
