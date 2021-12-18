c2aba67c6981f96f0be8263c1ef404f1962ef512
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-18 12:02:18 -0300

    consultando a existencia do usuario no banco

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -10,8 +11,36 @@ class LoginController extends Controller
     public function index(){
         return view('site.login', ['titulo' => 'login']);
     }
-    public function autenticar(){
-        return 'Chegamos até aqui !';
+    public function autenticar(Request $request){
+
+        //regras de validação
+
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //mensagens de feedback da validação
+
+        $feedback = [
+            'usuario.email' => 'O campo usuario precisa ser um email e é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+        
+        $request->validate($regras, $feedback); 
+
+        $email = $request->get('usuario');
+        $senha = $request->get('senha');
+
+        echo "Usuario : $email <br> Senha:$senha";
+
+        //iniciar o Model User
+        $user = new User();
+        $existe = $user->where('email', '=', $email)->where('password', '=', $senha)->get()->first();
+
+        echo "<pre>";
+        print_r($existe);
+        echo "</pre>";
     }
 }
 
--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -11,8 +11,10 @@
                 <div style="width:30%; margin-left:auto; margin-right:auto;">
                         <form action="{{route('site.login')}}" method="POST">
                             @csrf
-                            <input type="text" placeholder="usuário" class="borda-preta">
-                            <input type="password" placeholder="senha" class="borda-preta">
+                            <input name="usuario" value="{{old('usuario')}}" type="text" placeholder="usuário" class="borda-preta">
+                                {{$errors->has('usuario') ? $errors->first('usuario'):''}}
+                            <input name="senha" value="{{old('senha')}}" type="password" placeholder="senha" class="borda-preta">
+                                {{$errors->has('senha') ? $errors->first('senha'):''}}
                             <button type="submit" class="borda-preta">Acessar</button>
                         </form>
                 </div>
